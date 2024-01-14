cb43550b0eae7f900fb9a6a3110b2ecacf756e45
IsraelP2023 <portillosarmientoisrael@gmail.com>
2024-01-14 03:41:34 -0300

    4to commit

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 // script.js
 
+<script>
+    document.getElementById('contactForm').addEventListener('submit', function(event){
+        event.preventDefault(); // Previene el envío tradicional del formulario
+
+        var nombre = document.getElementById('nombre').value;
+        var email = document.getElementById('email').value;
+        var mensaje = document.getElementById('mensaje').value;
+
+        // Aquí puedes hacer lo que necesites con los datos, como enviarlos a una API
+        // Por ahora, solo mostraré una alerta
+        alert("Gracias, " + nombre + ". Tu mensaje ha sido enviado.");
+
+        // Opcional: Limpia el formulario después del envío
+        document.getElementById('contactForm').reset();
+    });
+</script>
